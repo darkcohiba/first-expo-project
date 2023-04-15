b33cb0d526251b5349851312d07a9473aad0e88b
Sam <samuel.waters@gmail.com>
2023-04-15 17:34:16 -0600

    chore: added tailwind and react navigation, both are functional

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,36 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 import React from 'react'
 
 const Homepage = () => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>This updated on my phone!</Text>
-            <Text style={styles.text}>WOW</Text>
+        <View className="items-center bg-red-800 border-black border-4 rounded-md h-3/4">
+            <View className="bg-black rounded-md m-2">
+                <Text className="text-red-400 text-center w-3/4  m-4">This updated on my phone!</Text>
+            </View>
+            <Text className="text-center text-purple-800 bg-green-400 w-1/2">WOW</Text>
             <StatusBar style="auto" />
         </View>
     );
 }
     
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: 'pink',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderWidth: 20,
-        borderColor: "black",
-        borderRadius: 50,
-      },
-      text: {
-        backgroundColor: 'black',
-        color: 'white',
-        borderWidth: 20,
-        borderRadius: 10,
-        overflow: "hidden",
-        opacity: 10,
-      }
-    });
+    // const styles = StyleSheet.create({
+    //   container: {
+    //     flex: 1,
+    //     backgroundColor: 'pink',
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     borderWidth: 20,
+    //     borderColor: "black",
+    //     borderRadius: 50,
+    //   },
+    //   text: {
+    //     backgroundColor: 'black',
+    //     color: 'white',
+    //     borderWidth: 20,
+    //     borderRadius: 10,
+    //     overflow: "hidden",
+    //     opacity: 10,
+    //   }
+    // });
 
 export default Homepage;
     
