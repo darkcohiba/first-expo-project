0c8df9fc4f1d3494ea0e898c5bcd565bbbb2969d
Sam <samuel.waters@gmail.com>
2023-04-16 22:55:19 -0600

    chore: search bar

--- screens/components/Search.js
+++ screens/components/Search.js
@@ -1,15 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import { 
-    SearchIcon,
+    MagnifyingGlassIcon,
     AdjustmentsVerticalIcon
    } from "react-native-heroicons/outline"
 
 const Search = () => {
   return (
-    <View>
-      <View>
-        <Text>Search</Text>
+    <View className="flex-row items-center space-x-2 pb-2 mx-4">
+      <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+        <MagnifyingGlassIcon color="gray" size={20} />
+        <TextInput 
+            placeholder='Restaurants and Cuisines'
+            keyboardType='default'
+        />
       </View>
       <AdjustmentsVerticalIcon color="00CCBB" />
     </View>
