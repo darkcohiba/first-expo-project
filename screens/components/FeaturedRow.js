7be07b8473ce488068a364fecd1d004ce1abc990
Sam <samuel.waters@gmail.com>
2023-04-17 11:02:09 -0600

    chore: created restaurant cards

--- screens/components/FeaturedRow.js
+++ screens/components/FeaturedRow.js
@@ -1,13 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { 
+    ArrowRightIcon
+} from "react-native-heroicons/outline"
 
 const FeaturedRow = ({title, description}) => {
   return (
     <View>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
+        <View className="mt-4 flex-row items-center justify-between px-4">
+            <Text className="font-bold text-lg ">{title}</Text>
+            <ArrowRightIcon color="#00CCBB"/>
+        </View>
+        <Text className="text-xs text-gray-500 px-4">{description}</Text>
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+                paddingHorizontal: 15,
+            }}
+            className="pt-4"
+        >
+            {/* Restuarant Cards */}
+        </ScrollView>
     </View>
   )
 }
 
 export default FeaturedRow
+
+
