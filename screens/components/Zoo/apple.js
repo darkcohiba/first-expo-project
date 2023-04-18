7d55175baef7791e299b525eb366336ee07ade7c
Sam <samuel.waters@gmail.com>
2023-04-18 11:03:07 -0600

    m

--- screens/components/Zoo/apple.js
+++ screens/components/Zoo/apple.js
@@ -5,3 +5,15 @@ export default function apple() {
     <div>apple</div>
   )
 }
+ import { View, Text } from 'react-native'
+ import React from 'react'
+ 
+ const apple = () => {
+   return (
+     <View>
+       <Text>apple</Text>
+     </View>
+   )
+ }
+ 
+ export default apple
